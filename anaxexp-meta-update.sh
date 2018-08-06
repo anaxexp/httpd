4a2033f3f2adf49c41e3f23db12879d196e120fe
AnaxExp Robot <vagrant@anax-laptop.localdomain>
2018-08-06 13:23:20 -0700

    update: fixes

--- anaxexp-meta-update.sh
+++ anaxexp-meta-update.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-cp 2.4/alpine/Dockerfile 2.4/alpine/Dockerfile.wodby
+cp 2.4/alpine/Dockerfile 2.4/alpine/Dockerfile.anaxexp
 
 # Change basic image.
-sed -i '/FROM alpine/i\ARG BASE_IMAGE_TAG\n' 2.4/alpine/Dockerfile.wodby
-sed -i 's/FROM alpine.*/FROM wodby\/alpine:${BASE_IMAGE_TAG}/' 2.4/alpine/Dockerfile.wodby
+sed -i '/FROM alpine/i\ARG BASE_IMAGE_TAG\n' 2.4/alpine/Dockerfile.anaxexp
+sed -i 's/FROM alpine.*/FROM wodby\/alpine:${BASE_IMAGE_TAG}/' 2.4/alpine/Dockerfile.anaxexp
 
-fullVersion=$(grep -oP '(?<=^ENV HTTPD_VERSION )([0-9\.]+)' 2.4/alpine/Dockerfile.wodby)
+fullVersion=$(grep -oP '(?<=^ENV HTTPD_VERSION )([0-9\.]+)' 2.4/alpine/Dockerfile.anaxexp)
 minorVersion=$(echo "${fullVersion}" | grep -oE '^[0-9]+\.[0-9]+')
 
 # Update travis.yml
